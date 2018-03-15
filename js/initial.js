22909b64200b05b935cf8dcf64aa297bcc06319b
C4R15 <chrispercevalmaxwell@gmail.com>
2018-03-15 21:22:22 +0000

    added html5 branch to remove an ITS repo

---
+++ js/initial.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".a-collapse").sideNav();
+	$(".dropdown-button").dropdown();
+	$('#toggleftauc').click(function() {
+		$("#featuredaucContainer").slideToggle('fast');
+	});
+	$('#toggleliveaucs').click(function() {
+		$("#liveaucsContainer").toggle('slide');
+	});
+	$('#toggleupcomingaucs').click(function() {
+		$("#upcomingaucsContainer").toggle('slide');
+	});
+});
